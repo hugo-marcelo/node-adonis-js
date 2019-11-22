f5b78c9362c562e8d0df1c25d23e52571dd1e184
Hugo Marcelo e Silva <hugo.marcelo@lg.com.br>
2019-11-21 22:08:07 -0300

    created relationships on projects, tasks and users

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,5 +1,6 @@
 'use strict'
 
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 const Hash = use('Hash')
 
@@ -16,6 +17,14 @@ class User extends Model {
 
   tokens () {
     return this.hasMany('App/Models/Token')
+  }
+
+  projects () {
+    return this.hasMany('App/Models/Project')
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Tasks')
   }
 }
 
